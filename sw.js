d17affe5793ad21b4beb46c5f416211dfb916957
christsiagkarlis-byte <christsiagkarlis@gmail.com>
2026-08-27 06:49:00 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = "paizomath-portable-v1";
+const CORE = ["./", "./index.html", "./css/styles.css", "./js/app.js", "./manifest.webmanifest"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE))));
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match("./index.html"))));
+});
