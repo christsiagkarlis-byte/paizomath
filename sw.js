e31f4376c4475abb40f12490b029a3b351c9b536
christsiagkarlis-byte <christsiagkarlis@gmail.com>
2026-08-27 13:25:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "paizomath-portable-v9";
-const CORE = ["./", "./index.html", "./terms.html", "./css/styles.css", "./css/terms.css", "./css/intro-topics.css", "./js/app.js", "./manifest.webmanifest", "./audio/paizomath-presentation-bed.mp3"];
+const CACHE = "paizomath-portable-v11";
+const CORE = ["./", "./index.html", "./terms.html", "./css/styles.css", "./css/terms.css", "./css/intro-topics.css", "./css/intro-safe.css", "./js/app.js", "./manifest.webmanifest", "./audio/paizomath-presentation-bed.mp3"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE))));
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", event => {
